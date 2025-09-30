f07cbbeae9e3953028eecbac4c31f299976f0d21
Ulug'bek Sotvoldiyev <dilafruztoshmatova209@gmail.com>
2025-09-30 22:30:14 +0500

    edit fetch

--- src/Request/index.js
+++ src/Request/index.js
@@ -1,31 +1,27 @@
-// import fetchWeatherMock from "./fetchWeatherMock";
-export const cities = ["London","New York", "Tokyo", "Sydney", "Cairo", ];
+export const cities = ["London", "New York", "Tokyo", "Sydney", "Cairo","Tashkent", "Fergana"];
 
-const API_KEY = '53157b6f30aa54d7005e13b012938bf4';
+const API_KEY = "53157b6f30aa54d7005e13b012938bf4";
 
-let lastApiCallTime = 0; // so'nggi chaqiruv vaqti (timestamp, ms)
+let lastApiCallTime = 0;
 
 export async function getCountryWeather(city) {
   const now = Date.now();
-  const THROTTLE_INTERVAL = 5000; // 5 soniya
+  const THROTTLE_INTERVAL = 5000;
 
   if (now - lastApiCallTime < THROTTLE_INTERVAL) {
-    // 5 soniyadan kam vaqt o'tgan, chaqiruv rad etiladi
     return Promise.reject(new Error("Iltimos, API chaqiruvlari orasida 5 soniyadan kam vaqt bo‘lmasin"));
   }
 
   lastApiCallTime = now;
 
-  // if (import.meta.env.MODE === "development") {
-  //   return fetchWeatherMock(city);
-  // }
-  if(import.meta.env.MODE === "development"){
-    try {
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
-      if (!res.ok) throw new Error("API error");
-      return await res.json();
-    } catch (error) {
-      throw new Error(`Ma'lumotni olishda xatolik yuz berdi: ${error.message}`);
-    }
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+    );
+
+    if (!res.ok) throw new Error("API error");
+    return await res.json();
+  } catch (error) {
+    throw new Error(`Ma'lumotni olishda xatolik yuz berdi: ${error.message}`);
   }
 }
